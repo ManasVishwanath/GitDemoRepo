1e75f1ddb088a8a13e635c89dff9f8765bb2c356
Manas Vishwanath Sira <manas.vishwanath.sira@ibm.com>
2025-11-18 22:35:34 -0600

    updated first.py

--- First.py
+++ First.py
@@ -1,8 +1,17 @@
-class TestCapture:
-    def __init__(self):
-        self.capture = {}
 
-    def put(self,key,value):
-        self.capture[key] = value
-    def get(self,key):
-        return self.capture[key]
+
+inputString = "sdjsiofewiohfdbvuehfsdbvdspS SHDJKDSF DFHJDFKS 83749HDFD "
+#gIVE ME CHARACTER WHICH IS COMING HIGHEST nO OF TIMES IN ABOVE STRING
+
+k = {}
+
+for char in inputString:
+    if char in k.keys():
+        k[char] = k[char] + 1
+    else:
+        k[char] = 1
+
+
+for i in k.values():
+
+
